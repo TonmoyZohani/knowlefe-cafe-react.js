1c03f888c791f29a060eadbac3bd7583b6f3f04b
tonmoy-zohani <tonmoyzohani@gmail.com>
2024-01-08 22:19:45 +0600

    counter function created

--- src/assets/components/Counter.jsx
+++ src/assets/components/Counter.jsx
@@ -1,22 +1,32 @@
 import React, { useReducer } from "react";
 
 const reducer = (state, action) => {
-  console.log(state, action);
-  return state + action;
+  console.log(state, action.payLoad);
+
+  switch (action.type) {
+    case "inc":
+      return state + action.payLoad;
+    case "dec":
+      return state + action.payLoad;
+  }
 };
 
 const Counter = () => {
   const [state, dispatch] = useReducer(reducer, 0);
 
   const handleIncrement = () => {
-    dispatch(1);
+    dispatch({ type: "inc", payLoad: 1 });
+  };
+
+  const handleDecrement = () => {
+    dispatch({ type: "dec", payLoad: -1 });
   };
 
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <p>{state}</p>
       <button onClick={handleIncrement}>Increment</button>
-      <button>Decrement</button>
+      <button onClick={handleDecrement}>Decrement</button>
       <button>Reset</button>
     </div>
   );
